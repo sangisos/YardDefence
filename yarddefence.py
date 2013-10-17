1124c63696477b482c6efd42fa7153d40b768b66
danielekbom <daniel5.2@hotmail.com>
2013-10-17 16:22:16 +0200

    merge

--- yarddefence.py
+++ yarddefence.py
@@ -83,8 +83,7 @@ class Menu(GameObject):
 		x,y = canvas.width/2,canvas.height/2
         GameObject.__init__(self,canvas,x,y,'c')
         Button(canvas,x,y-50,"Play")
-            
-            
+
 class Button(GameObject):
     def __init__(self,canvas,x,y,text):
         GameObject.__init__(self,canvas,x,y,'c')

4e97d1f4310e326e8d193a40d260e7cc3d161fa1
danielekbom <daniel5.2@hotmail.com>
2013-10-17 22:55:16 +0200

    Lagat CreateEnemy med difficulty

--- yarddefence.py
+++ yarddefence.py
@@ -11,54 +11,47 @@ from gameobject import GameObject
 import heroLife
 
 class GameWindow(Canvas):
-    '''Spelfönstrets klass, håller koll!'''
-    def __init__(self):
-        
-        # Runs in fullscreen
-        root.attributes('-fullscreen', True)
-        
-        difficulty = 1
-        self.width = root.winfo_screenwidth()
-        self.height = root.winfo_screenheight()
-        Canvas.__init__(self, root, width=self.width, height=self.height,bg="#009900")
-        self.pack()
-        
-        root.update()
-        
-        self.menu() 
-        
-        
-        
-        
-    def menu(self):
+	'''Spelfönstrets klass, håller koll!'''
+	difficulty = 1
+	
+	def __init__(self):
+	
+		# Runs in fullscreen
+		root.attributes('-fullscreen', True)
+		
+		self.width = root.winfo_screenwidth()
+		self.height = root.winfo_screenheight()
+		Canvas.__init__(self, root, width=self.width, height=self.height,bg="#009900")
+		self.pack()
+
+		root.update()
+		
+		self.menu()
+		
+		
+	def menu(self):
 		self.level=1
 		Menu(self)
 
 
-    def initGame(self,difficulty):
-		print difficulty
+	def initGame(self,difficulty):
 		self.background=Background(self)
 		self.activeEnemys=[]
 		self.deadEnemys=[]
 		self.after(0,self.doOneFrame)
 		self.after(0,self.createEnemy)
-        
-    def createEnemy(self):
-        self.activeEnemys.append(enemy2(self))
-        self.after(randint(100,3000)/(self.level*self.difficulty),self.createEnemy)
-        
-    
-    def doOneFrame(self):
-        # Commented for debug. Uncomment to loop: 
-        self.after(30,self.doOneFrame)
-        ### Skriv kod efter här för att visa en frame
-        
-        
-        
-        
-        
-        root.update()
-        
+		
+	def createEnemy(self):
+		self.activeEnemys.append(enemy2(self))
+		self.after(randint(100,3000)/(self.level*self.difficulty),self.createEnemy)
+		
+	def doOneFrame(self):
+		# Commented for debug. Uncomment to loop: 
+		self.after(30,self.doOneFrame)
+		### Skriv kod efter här för att visa en frame
+		
+		root.update()
+		
     
 class Enemy(GameObject):
     '''Subclasses should have images numbered correct in a subfolder to images named <classname> in all lowercase characters. The folder should also contain a "dissabled image" that should be in alpabetically last order. These images will be avalible as ImageTk.PhotoImage objects in the variables 'images' and 'eliminatedImage' respectivly.'''
@@ -98,7 +91,8 @@ class Menu(GameObject):
 		hardButton.canvas.tag_bind(hardButton.imageID,'<Button-1>',self.difficultyButtonClick)
 	
 	def difficultyButtonClick(self,event):
-		self.canvas.initGame(2)
+		self.difficulty = 2
+		self.canvas.initGame(self.difficulty)
 			
 class Button(GameObject):
 	def __init__(self,canvas,x,y,text):

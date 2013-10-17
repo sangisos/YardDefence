47e029a82b71c10ed9ef347a694995fadf10830d
Alexander Andersson <alexander@samproduktionab.se>
2013-10-17 15:19:57 +0200

    Oj, missade adda gameobject, nu så\!

---
+++ gameobject.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+try:  # import as appropriate for 2.x vs. 3.x
+    from tkinter import Tk,Canvas,Toplevel
+except:
+    from Tkinter import Tk,Canvas,Toplevel
+from PIL import Image,ImageTk
+import os
+from random import randint
+
+class MetaGameObject(type):
+    def __new__(mcs, classname, bases, dictionary):
+        if not classname in ["GameObject", "Enemy"]:
+            imageDir=os.curdir + os.sep + "images" + os.sep + str(classname).lower()
+            filenames=[os.path.join(imageDir, f) for f in os.listdir(imageDir) if f.endswith(".gif")]
+            filenames.sort()
+            if "Enemy" in bases:
+                dictionary["eliminatedImage"]=ImageTk.PhotoImage(image=Image.open(filenames.pop()))
+            dictionary["images"] = [ImageTk.PhotoImage(image=Image.open(filename)) for filename in filenames]
+            
+        return type.__new__(mcs, classname, bases, dictionary)
+
+class GameObject:
+    '''All object that show pictures on screen should inherit the GameObjectclass, they have to have a folder in the image folder namnet <classname>'''
+    __metaclass__=MetaGameObject
+    
+    def __init__(self,canvas,x,y):
+        self.canvas=canvas
+        self.imageID = self.canvas.create_image(x,y,anchor="nw",image=self.__class__.images[0])

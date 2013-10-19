9cd3a01dd174cb19fab13c082cb6f6892d8d9b81
danielekbom <daniel5.2@hotmail.com>
2013-10-19 18:47:15 +0200

    justerat hero

--- hero.py
+++ hero.py
@@ -3,4 +3,7 @@ from gameobject import *
 
 class Hero(GameObject):
 	def __init__(self,game):
-		GameObject.__init__(self,game,125,game.height/2.5,'c')
+		GameObject.__init__(self,game,125,game.height/2.5,'c',callback=self.heroShoot)
+	
+	def heroShoot(self,event):
+		print "Shooooting"

707d53c184bc61a0e048020f4aa3def0059b439d
Alexander Andersson <sangisos@gmail.com>
2013-10-21 11:29:33 +0200

    Felsöker varför fiender inte raderas

--- enemybaseclasses.py
+++ enemybaseclasses.py
@@ -10,13 +10,16 @@ class Enemy(GameObject):
     hp=1
     movePixels=-1
     eliminated=True
+    activeEnemies=[]
+    deadEnemies=[]
+    
     def __init__(self,game):
         self.eliminated = False
         self.game=game
         self.hp=self.getStartHp()
-        self.positionOfTailX=game.width+self.getWidth()
-        
-        x,y=game.width-50,randint(100,game.height-100)
+        self.positionOfTailX=game.winfo_width()+self.getWidth()
+        margintopbottom=game.winfo_height()/7
+        x,y=game.winfo_width()-50,randint(margintopbottom,margintopbottom*6)
         
         self.tag="enemy"+str(hash(self))
         
@@ -30,6 +33,8 @@ class Enemy(GameObject):
         
         self.game.tag_bind(self.tag,'<Button-1>',self.callback)
         
+        self.activeEnemies.append(self)
+        
         game.after(1,self.walk)
         game.after(1,self.animate)
         
@@ -37,10 +42,14 @@ class Enemy(GameObject):
         print "enemy deleted i enemybaseclass.__del__("+str(self)+")"
         
     def delete(self):
-        try:
-            self.game.deleteEnemy(self)
+  #      try:
+            #self.game.deleteEnemy(self)
+            self.activeEnemies.remove(self)
             if not self.eliminated:
                 self.eliminated=True
+                for o in self.objectIds:
+                    self.game.tag_unbind(o,'<Button-1>')
+                    self.game.delete(o)
                 self.game.tag_unbind(self.tag,'<Button-1>')
                 del self.objectIdsCycle
                 del self.objectIds
@@ -48,25 +57,24 @@ class Enemy(GameObject):
             self.game.delete(self.tag)
             self.game.delete(self.objectId)
             del self.callback
-            del self.walk
             del self.tag
             del self.hp
             del self.objectId
             del self.eliminated
-        except Exception, e:
-            print "fel i eliminate: " + e
+#        except Exception, e:
+ #           print "fel i ebc.delete: " + str(e)
         #self.__del__()
     
             
     def eliminate(self):
         x,y = self.game.coords(self.tag)
         self.eliminated=True
-        self.game.killEnemy(self)
+        #self.game.killEnemy(self)
         try:
             self.game.tag_unbind(self.tag,'<Button-1>')
             self.game.delete(self.tag)
         except:
-            print "fel i eliminate"
+            print "fel i ebc.eliminate"
         del self.objectIdsCycle
         del self.objectIds
         self.objectId=self.game.create_image(x,y,anchor='sw',image=self.eliminatedImage,tags=("Dead"))
@@ -101,13 +109,12 @@ class Enemy(GameObject):
     def walk(self):
         if self.game.gamePaused:
             self.game.resumeQueue.append(self.walk)
-        elif not self.eliminated:
-            if self.positionOfTailX>0:
-                self.game.after(int(100/self.speed),self.walk)
-                self.move(self.movePixels,0)
-                self.positionOfTailX=self.positionOfTailX+self.movePixels
-            else:
-                self.delete()
+        elif not self.eliminated and self.positionOfTailX>0:
+            self.game.after(int(100/self.speed),self.walk)
+            self.move(self.movePixels,0)
+            self.positionOfTailX=self.positionOfTailX+self.movePixels
+        else:
+            self.delete()
         
     def nextPicture(self):
         self.game.itemconfig(self.objectId,state='hidden')

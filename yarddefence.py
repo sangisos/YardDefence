8ec07afd917a4cb3056d06bdf2315dc14cee0fe0
danielekbom <daniel5.2@hotmail.com>
2013-10-22 11:26:09 +0200

    storyteller visar rätt text beroende på level
    Även provat alla texter så att de passar bra på storyteller background.

--- yarddefence.py
+++ yarddefence.py
@@ -115,16 +115,17 @@ class GameWindow(Canvas):
 
 class StoryTeller(GameObject):
 	def __init__(self,game):
-            #self.tag="storyTeller" + str(hash(self))
+            self.level1Text = "Dear Neighbour, \nYesterday my whole farm was attacked by a massive mob of \nWILD ANIMALS, they have eaten all my harvest. \nI am afraid that they are on their way to your farm right now. \nI hope you are prepared to protect your land! \n\nRegards,\nLennart"
+            self.level2Text = "Oh my good, these animals are CRAZY! \nI have an idea, lets dig a river through our lands, \nmaybe that will keep the animals away. \n\nRegards, \nLennart"
+            self.level3Text = "Looks like the river is to small for theese even \ncrazier animals. \nBut hey, I have an even CRAZIER idea. \nI have some dynamites in my basement, lets use them \nto blow up some more land. \nIm sure that will stop the animals from destroying our farms. \nGood luck! \n\nRegards, \nLennart"
+            self.victoryText = "If you read this I assume that you are still alive \nand have defeated the CRAZY big boat of wild animals. \nI saw them, they loaded the boat with all the remainings \nof their army. Our farms are safe now. \nCongratulations! \n\nGreetings, \nLennart"
+            self.storyTexts = [self.level1Text,self.level2Text,self.level3Text,self.victoryText]
             game.background=Background(game)
             x,y = game.width/2,game.height/2
             GameObject.__init__(self,game,x,y,'c',self.storyTellerOnClick)
-            self.textObject=Text(game,x,y,text="Dear Neighbour, \nYesterday my whole farm was attacked by a massive mob of \nWILD ANIMALS, they have eaten all my harvest. \nI am afraid that they are on their way to your farm right now. \nI hope you are prepared to protect your land! \n\nRegards,\nLennart",callback=self.storyTellerOnClick,color='black',font=game.storyFont)
-            #Oh my good, these animals are CRAZY! \nI have an idea, lets dig a river through our lands, \nmaybe that will keep the animals away. \n\nRegards, \nLennart"
-            #Looks like the river is to small for theese even crazier animals. \nBut hey, I have an even CRAZIER idea. \nI have some old dynamites in my basement, lets use them to blow up some more land. \Im sure that will stop the animals from destroying our farms. \nGood luck! \n\nRegards, \nLennart
-            #If you read this I assume that you are still alive \nand have defeated the CRAZY big boat of wild animals. \nI saw them, they loaded the boat with all the remainings \of their army. Our farms are now safe. \nCongratulations! \n\nGreetings, \nLennart \n\nScore: " + currentScore
+            self.textObject=Text(game,x,y,text=self.storyTexts[game.level-1],callback=self.storyTellerOnClick,color='black',font=game.storyFont)
+            
         def storyTellerOnClick(self,event):
-            #self.game.delete(self.tag)
             self.game.delete(self.objectId)
             del self.textObject
             self.game.initGame()
